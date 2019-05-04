9c655174134b8c11bffabdd70f661f4636d17986
odun-lami <davidoduns1@gmail.com>
2019-05-04 15:19:41 +0100

    added date and title

--- newblog/blog/admin.py
+++ newblog/blog/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Posts)
+admin.site.register(Post)
--- newblog/blog/models.py
+++ newblog/blog/models.py
@@ -5,7 +5,8 @@ class Post(models.Model):
 
     title=models.CharField(max_length=70)
     body=models.TextField()
-    published=models.DateTimeField(auto_now=True)
+    published=models.DateTimeField()
 
+    def __str__(self):
 
-
+      return '{} published on {}'.format(self.title + self.published)
