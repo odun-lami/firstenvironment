6d44002ad112fb35a2441cb13160fd65dd3c6306
odun-lami <davidoduns1@gmail.com>
2019-05-18 12:26:25 +0100

    linking

--- newblog/blog/models.py
+++ newblog/blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 class Post(models.Model):
-
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
     title=models.CharField(max_length=70)
     body=models.TextField()
     published=models.DateTimeField()
