b3248d1cc1c41b02c4c8627b8a88bb748b0db8f1
odun-lami <davidoduns1@gmail.com>
2019-06-15 12:17:47 +0100

    a whole  lot, man, read up

--- newblog/newblog/settings.py
+++ newblog/newblog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fhwq4ykw44b1=ya(3+0)jhql9_u!)c26-15(kx$4$qe&ib50ia'
+SECRET_KEY = config ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
